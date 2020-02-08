91f514ea4ca6e3df3f93661310d8208fcb865358
Andrew Ruether <aruether@gmail.com>
2020-02-07 19:05:25 -0500

    Added Google Form log

---
+++ recordsong.py
@@ -0,0 +1,26 @@
+import requests
+import sys	
+import configparser
+import os
+
+# Send the name of the song being played to a Google form
+# Usage: python recordsong.py <name of song>
+
+# set up a Google Form
+# Set the URL (minus the last /viewform 
+
+if len(sys.argv) < 2:
+	sys.exit()
+
+# Load in config.  Look for the config.ini in the same folder
+# as this script
+config = configparser.ConfigParser()
+config.read(os.path.dirname(os.path.realpath(__file__)) + '/config.ini')
+
+# Help from https://stackoverflow.com/a/55815520
+url =  config['RecordSong']['form_url'] + "/formResponse"
+form_data = {'entry.2022304067':sys.argv[1]}
+r = requests.post(url, data=form_data)
+print (r)
+print (r.status_code)
+print (r.text)
